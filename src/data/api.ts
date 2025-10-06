291b1b5df08df24fb1b028e4bcad4e2273427a0d
Kutub Udd <socialnetwork98710@gmail.com>
2025-10-06 10:47:15 +0600

    added

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,4 +1,1 @@
-export const api =
-  process.env.NODE_ENV === "production"
-    ? "https://next-spark-backend.vercel.app"
-    : "http://localhost:5000/api/v1";
+export const api = "https://next-spark-fr.vercel.app";
